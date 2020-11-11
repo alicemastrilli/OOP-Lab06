6df0d5f8fa4d5c1d100b1723b4fdce279b432c3f
Alice Mastrilli <alice.mastrilli@studio.unibo.it>
2020-11-11 09:01:57 +0100

    fatto ese 1(collections 1)

--- src/it/unibo/oop/lab/collections1/UseCollection.java
+++ src/it/unibo/oop/lab/collections1/UseCollection.java
@@ -1,11 +1,16 @@
 package it.unibo.oop.lab.collections1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Example class using {@link java.util.List} and {@link java.util.Map}.
  * 
  */
 public final class UseCollection {
-
+	private static final int ELEM = 100000;
+    private static final int TO_MS = 1_000_000;
     private UseCollection() {
     }
 
@@ -14,23 +19,46 @@ public final class UseCollection {
      *            unused
      */
     public static void main(final String... s) {
-        /*
-         * 1) Create a new ArrayList<Integer>, and populate it with the numbers
-         * from 1000 (included) to 2000 (excluded).
-         */
-        /*
+    	ArrayList<Integer> arr = new ArrayList<>();
+    	int count=0;
+    	for(count=1000 ; count<2000 ; count++) {
+    		arr.add(count);
+    	}
+    	//System.out.println(arr);
+    	/*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+    	LinkedList<Integer> list = new LinkedList<>();
+    	list.addAll(arr);
+    	
+    	//System.out.println(list);
+        /*
+         * 1) Create a new ArrayList<Integer>, and populate it with the numbers
+         * from 1000 (included) to 2000 (excluded).
+         */
+        
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+    	
+    	int primo = arr.get(0);
+    	int ultimo = arr.get(arr.size() -1 );
+    	arr.set(0, ultimo);
+    	arr.set(arr.size() - 1, primo);
+    	
+    	//System.out.println(arr);
+    	
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+    	
+    	for(int i : arr) {
+    		System.out.println(i);
+    	}
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -38,12 +66,29 @@ public final class UseCollection {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+    	long time = System.nanoTime();
+    	
+    	for (int i=0 ; i < ELEM ; i++) {
+    		arr.add(i);
+    		list.add(i);
+    	}
+    	
+    	time = System.nanoTime()-time;
+    	System.out.println(time/TO_MS + "ms");
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+    	
+    	long time2 = System.nanoTime();
+    	for (int i = 0 ; i< 1000 ; i++) {
+    		arr.get((arr.size())/2);
+    		list.get((list.size())/2);
+    	}
+    	time2=System.nanoTime()-time2;
+    	System.out.println(time2/TO_MS + "ms");
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -60,8 +105,21 @@ public final class UseCollection {
          * 
          * Oceania -> 38,304,000
          */
+    	HashMap<String, Long> continenti = new HashMap<>(); 
+    	continenti.put("Africa",(long) 1110635000 );
+    	continenti.put("America",(long) 972005000 );
+    	continenti.put("Antartride",(long) 0 );
+    	continenti.put("Asia",(long ) 4298723000L );
+    	continenti.put("Europa",(long) 742452000 );
+    	continenti.put("Oceania",(long) 38304000 );
         /*
          * 8) Compute the population of the world
          */
+    	long abit=0;
+    	for(long elem : continenti.values()) {
+    		abit = abit + elem;
+    	}
+    	System.out.println(abit);
+    	
     }
 }
